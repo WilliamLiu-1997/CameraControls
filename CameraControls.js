8dc34addb98596a9fb67aaf8adb7a8fbb3e3ab5d
WilliamLiu-1997 <77861330+WilliamLiu-1997@users.noreply.github.com>
2021-08-10 02:18:58 +1000

    Update CameraControls.js

--- CameraControls.js
+++ CameraControls.js
@@ -11,11 +11,11 @@ import {
 // This set of controls performs turning, dollying (zooming), and panning. It is an update of OrbitControls
 // Pan up / down / left / right  - right mouse, or WASD keys / touch: one finger move
 // Move forward / backward  - mousewheel or WASD keys / touch: two finger spread or squish
-// Turn  - middle mouse, or arrow keys / touch: three finger swipe
+// Rotate  - middle mouse, or arrow keys / touch: three finger swipe
 
 // Updates compared to OrbitControls:
 // 1. The dollying of PerspectiveCamera is replaced with panning forward and backward.Therefore, this component can be used to move the PerspectiveCamera to six directions including forward, backward, up, down, left and right from current perspective.
-// 2. The turning will be conducted using the PerspectiveCamera as the center.
+// 2. The rotation will be conducted using the PerspectiveCamera as the center.
 
 class CameraControls extends EventDispatcher {
 
